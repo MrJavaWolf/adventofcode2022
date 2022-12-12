165afa9c80584a7b1d9020c072d932281e4eeb9c
Jens Larsen <jepl@dictus.dk>
2022-12-12 16:55:50 +0100

    Added: Day 12

---
+++ adventofcode2022/day12/AStarGrid.cs
@@ -0,0 +1,169 @@
+﻿namespace adventofcode2022.day12
+{
+    public class Node
+    {
+        // Change this depending on what the desired size is for each element in the grid
+        public Node? Parent;
+        public int PositionX;
+        public int PositionY;
+        public float DistanceToTarget;
+        public float Cost;
+        public float Weight;
+        public char Elevation;
+        public float F
+        {
+            get
+            {
+                if (DistanceToTarget != -1 && Cost != -1)
+                    return DistanceToTarget + Cost;
+                else
+                    return -1;
+            }
+        }
+        public Node(int positionX, int positionY, char type)
+        {
+            Parent = null;
+            PositionX = positionX;
+            PositionY = positionY;
+            DistanceToTarget = -1;
+            Cost = 1;
+            Weight = 1;
+            Elevation = type;
+        }
+
+        public override string ToString()
+        {
+            return $"({PositionX},{PositionY}) {Elevation}";
+        }
+    }
+
+    public class Astar
+    {
+        Node[,] Grid;
+        int GridCols => Grid.GetLength(0);
+        int GridRows => Grid.GetLength(1);
+
+        public Astar(char[,] grid)
+        {
+            Grid = new Node[grid.GetLength(0), grid.GetLength(1)];
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GetLength(1); y++)
+                {
+                    char elevation = grid[x, y];
+                    if (elevation == 'S') elevation = 'a';
+                    if (elevation == 'E') elevation = 'z';
+                    Grid[x, y] = new Node(x, y, elevation);
+                }
+            }
+        }
+
+        public Stack<Node>? FindPath(int startX, int startY, int endX, int endY)
+        {
+
+            Node start = Grid[startX, startY];
+            Node end = Grid[endX, endY];
+
+            Stack<Node> Path = new Stack<Node>();
+            //PriorityQueue<Node, float> OpenList = new PriorityQueue<Node, float>();
+            Queue<Node> OpenList = new Queue<Node>();
+            List<Node> ClosedList = new List<Node>();
+            List<Node> adjacencies;
+            Node current = start;
+
+            // add start node to Open List
+            //OpenList.Enqueue(start, start.F);
+            OpenList.Enqueue(start);
+
+            while (OpenList.Count != 0 && !ClosedList.Exists(x => x.PositionX == end.PositionX && x.PositionY == end.PositionY))
+            {
+                //current = OpenList.Dequeue();
+                current = OpenList.Dequeue();
+                ClosedList.Add(current);
+                adjacencies = GetAdjacentNodes(current);
+
+                foreach (Node adjacentNode in adjacencies)
+                {
+                    if (!ClosedList.Contains(adjacentNode))
+                    {
+
+                        adjacentNode.Parent = current;
+                        adjacentNode.DistanceToTarget = Math.Abs(adjacentNode.PositionX - end.PositionX) + Math.Abs(adjacentNode.PositionY - end.PositionY);
+                        adjacentNode.Cost = adjacentNode.Weight + current.Cost;
+                        //OpenList.Enqueue(adjacentNode, adjacentNode.F);
+                        if (!OpenList.Contains(adjacentNode))
+                            OpenList.Enqueue(adjacentNode);
+                    }
+                    else
+                    {
+                        if (adjacentNode.Cost > adjacentNode.Weight + current.Cost)
+                        {
+                            adjacentNode.Parent = current;
+                            adjacentNode.Cost = adjacentNode.Weight + current.Cost;
+                            //OpenList.Enqueue(adjacentNode, adjacentNode.F);
+                            if (!OpenList.Contains(adjacentNode))
+                                OpenList.Enqueue(adjacentNode);
+                        }
+                    }
+                }
+            }
+
+            // construct path, if end was not closed return null
+            if (!ClosedList.Exists(x => x.PositionX == end.PositionX && x.PositionY == end.PositionY))
+            {
+                return null;
+            }
+
+            // if all good, return path
+            Node? temp = ClosedList[ClosedList.IndexOf(current)];
+            if (temp == null) return null;
+            do
+            {
+                Path.Push(temp);
+                temp = temp.Parent;
+            } while (temp != start && temp != null);
+            return Path;
+        }
+
+        private List<Node> GetAdjacentNodes(Node n)
+        {
+            List<Node> temp = new List<Node>();
+
+            int row = n.PositionY;
+            int col = n.PositionX;
+
+            if (row + 1 < GridRows)
+            {
+                temp.Add(Grid[col, row + 1]);
+            }
+            if (row - 1 >= 0)
+            {
+                temp.Add(Grid[col, row - 1]);
+            }
+            if (col - 1 >= 0)
+            {
+                temp.Add(Grid[col - 1, row]);
+            }
+            if (col + 1 < GridCols)
+            {
+                temp.Add(Grid[col + 1, row]);
+            }
+
+            // Check if we can walk to the Adjacent Node
+            for (int i = 0; i < temp.Count; i++)
+            {
+                if (!CanWalk(n, temp[i]))
+                {
+                    temp.RemoveAt(i);
+                    i--;
+                }
+            }
+            return temp;
+        }
+
+        private bool CanWalk(Node from, Node to)
+        {
+            return to.Elevation - from.Elevation <= 1;
+        }
+    }
+}
---
+++ adventofcode2022/day12/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace adventofcode2022.day12;
+
+class HeightMap
+{
+    public char[,] Map { get; set; }
+    public HeightMap(int x, int y)
+    {
+        Map = new char[x, y];
+    }
+
+    public (int x, int y) GetStartPosition()
+    {
+        return GetPosition('S');
+    }
+
+    public (int x, int y) GetEndPosition()
+    {
+        return GetPosition('E');
+    }
+
+    public List<(int x, int y)> FindAllScenicStartingPositions()
+    {
+        List<(int x, int y)> startingPositions = new();
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < Map.GetLength(1); j++)
+            {
+                if (Map[i, j] == 'a')
+                    startingPositions.Add((i, j));
+            }
+        }
+        return startingPositions;
+    }
+
+    public (int x, int y) GetPosition(char c)
+    {
+        for (int i = 0; i < Map.GetLength(0); i++)
+        {
+            for (int j = 0; j < Map.GetLength(1); j++)
+            {
+                if (Map[i, j] == c)
+                    return (i, j);
+            }
+        }
+
+        throw new Exception($"Did not find position with '{c}'");
+    }
+}
+
+internal class Program
+{
+
+    public static void Run()
+    {
+        Console.WriteLine();
+        Console.WriteLine("- - - - - Day 12 - - - - -");
+        HeightMap map = Load();
+        var startPosition = map.GetStartPosition();
+        var endPosition = map.GetEndPosition();
+        Astar astar = new Astar(map.Map);
+        var path = astar.FindPath(startPosition.x, startPosition.y, endPosition.x, endPosition.y);
+        Console.WriteLine($"Part 1: {path?.Count}");
+
+        //for (int y = 0; y < map.Map.GetLength(1); y++)
+        //{
+        //    for (int x = 0; x < map.Map.GetLength(0); x++)
+        //    {
+        //        Node? step = path?.Where(n => n.PositionX == x && n.PositionY == y).FirstOrDefault();
+        //        if (step != null)
+        //        {
+        //            Console.Write(".");
+        //        }
+        //        else
+        //        {
+        //            Console.Write(map.Map[x, y]);
+        //        }
+        //    }
+        //    Console.WriteLine();
+        //}
+        var bestPath = astar.FindPath(0, 26, endPosition.x, endPosition.y);
+        //var possibleStartingPositions = map.FindAllScenicStartingPositions();
+        //int minimum = int.MaxValue;
+        //Stack<Node>? bestPath = null;
+        //foreach (var possibleStartingPosition in possibleStartingPositions)
+        //{
+        //    //Console.Write(".");
+        //    var possiblePath = astar.FindPath(possibleStartingPosition.x, possibleStartingPosition.y, endPosition.x, endPosition.y);
+        //    if(possiblePath != null && possiblePath.Count < minimum)
+        //    {
+        //        minimum = possiblePath.Count;
+        //        bestPath = possiblePath;
+        //    }
+        //}
+        Console.WriteLine($"Part 2: {bestPath?.Count}");
+    }
+
+    public static HeightMap Load()
+    {
+        string inputFile = "day12/input";
+        string[] lines = File.ReadAllLines(inputFile);
+        HeightMap heightMap = new HeightMap(lines[0].Length, lines.Length);
+        for (int y = 0; y < lines.Length; y++)
+        {
+            for (int x = 0; x < lines[y].Length; x++)
+            {
+                heightMap.Map[x, y] = lines[y][x];
+            }
+        }
+        return heightMap;
+    }
+}
